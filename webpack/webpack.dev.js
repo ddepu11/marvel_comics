2c0ccde5bdadfb71540823b80d76958e5a1682c4
Deepanshu Tiwari <ddepu11@gmail.com>
2021-10-03 17:39:13 +0530

    chore: configure for dev

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -1,3 +1,13 @@
+const path = require("path");
+
 module.exports = {
   mode: "development",
+
+  devtool: "eval-cheap-module-source-map",
+
+  devServer: {
+    static: path.resolve(__dirname, "../build"),
+    port: 3000,
+    hot: true,
+  },
 };
