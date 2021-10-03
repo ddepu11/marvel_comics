b2e9c7aac9ffe95dc12e14a3e3725d123e675aba
Deepanshu Tiwari <ddepu11@gmail.com>
2021-10-03 17:05:33 +0530

    chore: add html-webpack-plugin support

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,12 +1,19 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "production",
-  
+
   entry: path.resolve(__dirname, "../src/index.js"),
 
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "../build"),
   },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "../src/index.html"),
+    }),
+  ],
 };
