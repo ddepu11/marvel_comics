288e883b5cb77389ed8290d8e79a82a2469fb28b
Deepanshu Tiwari <ddepu11@gmail.com>
2021-10-03 23:20:51 +0530

    fix: allow systemvars, env node true

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -9,7 +9,9 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, '../src/index.html'),
     }),
-    new Dotenv(),
+    new Dotenv({
+      systemvars: true,
+    }),
   ],
 
   module: {
